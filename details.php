234ddb39eb7be7ae18f8e47fdf3a28663f813f60
Alina Bozynski <abozynski@pm.me>
2022-08-16 21:26:27 -0500

    A comment system was added so that comments show up with the appropriate page

--- details.php
+++ details.php
@@ -2,6 +2,62 @@
 <?php    
     require('connect.php');
 
+    $emp_commments_query = "SELECT * from emp_comments";
+    $dept_comments_query = "SELECT * from dept_comments";
+
+    $emp_comment_statement = $db->prepare($emp_commments_query);
+    $dept_comment_statement = $db->prepare($dept_comments_query);
+
+    $emp_comment_statement->execute();
+    $dept_comment_statement->execute(); 
+
+    // Check if comment form was submitted and if a comment has been entered.
+    if(isset($_POST['add_comment']) && !empty($_POST['comment'])){
+
+        if(isset($_GET['emp_id'])){
+            // Sanitize user input to filter out dangerous characters and make sure 
+            //     they are valid to enter into the SQL.
+            $emp_id = filter_input(INPUT_GET, 'emp_id', FILTER_SANITIZE_NUMBER_INT);
+            $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+
+            // Build and prepare the parameterized SQL query and bind to the above sanitized values.
+            $query = "INSERT INTO emp_comments (emp_id, comment) VALUES (:emp_id, :comment)";
+            $statement = $db->prepare($query); 
+            $statement->bindValue(':emp_id', $emp_id);
+            $statement->bindValue(':comment', $comment);
+
+            // Execute the INSERT statement.
+            $statement->execute(); 
+
+            // Display message to show the user that the record addition was successful.
+                
+            header("Location: details.php?emp_id={$_GET['emp_id']}");
+
+        } elseif(isset($_GET['department_id'])){
+            // Sanitize user input to filter out dangerous characters and make sure 
+            //     they are valid to enter into the SQL.
+            $department_id = filter_input(INPUT_GET, 'department_id', FILTER_SANITIZE_NUMBER_INT);
+            $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+
+            // Build and prepare the parameterized SQL query and bind to the above sanitized values.
+            $query = "INSERT INTO dept_comments (department_id, comment) VALUES (:department_id, :comment)";
+            $statement = $db->prepare($query); 
+            $statement->bindValue(':department_id', ucfirst($department_id));
+            $statement->bindValue(':comment', $comment);
+
+            // Execute the INSERT statement.
+            $statement->execute(); 
+
+            // Display message to show the user that the record addition was successful.
+            
+            header("Location: details.php?department_id={$_GET['department_id']}");
+        }
+
+    } elseif(isset($_POST['add_comment'])){
+
+        echo "ATTENTION: Comment could not be added. Please ensure the comment is not blank.";
+    }    
+
     if(isset($_GET['emp_id'])){
         // Sanitize the id GET parameter.
         $emp_id = filter_input(INPUT_GET, 'emp_id', FILTER_SANITIZE_NUMBER_INT);
@@ -84,12 +140,31 @@
             <p><b>Phone: </b><?= $employee['tel_number'] ?></p>
             <p><b>Email: </b><?= $employee['email'] ?></p>
             <p><b>Record Updated On: </b><?= date_format(date_create($employee['updated']), "F d, Y, g:i a") ?></p>
-            <?php if($employee['image_file'] != null): ?>
+            <?php if($employee['image_file'] != null || !empty($employee['image_file'])): ?>
                 <p><img src="uploads/<?= $employee['image_file'] ?>" alt="<?= $employee['image_file'] ?>" title="<?= $employee['image_file'] ?>" height="300"></p>
             <?php endif ?>
         </div>
+
+        <form method="POST" action="details.php?emp_id=<?= $_GET['emp_id'] ?>">
+            <h3>Add comments about this employee</h3>
+            <label for="comment">Comments</label>
+            <input type="text" id="comment" name="comment">
+            <input type="submit" class="submit" name="add_comment" value="Add Comment">
+        </form>
+
+        <?php while($comments = $emp_comment_statement->fetch()): ?>
+            <?php if($comments['emp_id'] == $_GET['emp_id']): ?>
+                <div>
+                    <p><?= $comments['comment'] ?></p>
+                    <p><?= $comments['created'] ?></p>
+                </div>
+            <?php endif ?>
+        <?php endwhile ?>
+
     </body>
+    
 <?php elseif(isset($_GET['department_id'])): ?>
+
     <head>
         <title><?= $department['department_name'] ?></title>
         <link href='https://fonts.googleapis.com/css2?family=Rubik+Moonrocks&display=swap&family=Shadows+Into+Light&family=Space+Mono' rel='stylesheet' type='text/css'>
@@ -106,7 +181,7 @@
             <p><b>Phone: </b><?= $department['tel_number'] ?></p>
             <p><b>Email: </b><?= $department['email'] ?></p>
             <p><b>Record Updated On: </b><?= date_format(date_create($department['updated']), "F d, Y, g:i a") ?></p>
-            <?php if($department['image_file'] != null): ?>
+            <?php if($department['image_file'] != null || !empty($department['image_file'])): ?>
                 <p><img src="uploads/<?= $department['image_file'] ?>" alt="<?= $department['image_file'] ?>" title="<?= $department['image_file'] ?>" height="300"></p>
             <?php endif ?>
         </div>
@@ -117,6 +192,24 @@
                 <p><a href="details.php?emp_id=<?= $employee_info['emp_id'] ?>"><?= $employee_info['first_name'] ?> <?= $employee_info['last_name'] ?></a></p>
             <?php endwhile ?>
         </div>
+
+        <form method="POST" action="details.php?department_id=<?= $_GET['department_id'] ?>">
+            <h3>Add comments about this department</h3>
+            <label for="comment">Comments</label>
+            <input type="text" id="comment" name="comment">
+            <input type="submit" class="submit" name="add_comment" value="Add Comment">
+        </form>
+
+        <?php while($comments = $dept_comment_statement->fetch()): ?>
+            <?php if($comments['department_id'] == $_GET['department_id']): ?>
+                <div>
+                    <p><?= $comments['comment'] ?></p>
+                    <p><?= $comments['created'] ?></p>
+                </div>
+            <?php endif ?>
+        <?php endwhile ?>
+
     </body>
+    
 <?php endif ?>
 </html> 
